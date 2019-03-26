030bed4dc68c35e52bfb34a2e811dcf433edd207
gordontsui <tsui.gordon.9@gmail.com>
2019-03-26 15:29:26 -0700

    added more javascript for reveal phrase

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ function createButtons() {
         document.getElementById("buttons").appendChild(btn);
         btn.innerHTML = String.fromCharCode(i + 64);
         btn.class = "letters";
+        btn.id = String.fromCharCode(i + 64);
         btn.style.borderRadius = "4px";
         btn.style.backgroundColor = "#AFEEEE";
         btn.style.width = "35px";
         btn.style.height = "30px";
 
         btn.addEventListener("click", function() {
-            console.log("Button " + String.fromCharCode(i + 64) + " was clicked")
+            console.log("Button " + String.fromCharCode(i + 64) + " was clicked");
+            btn.disabled = true;
+            console.log(checkCharacter(btn.innerHTML));
+            revealCharacter(btn.innerHTML);
+
         });
     };
 };
@@ -21,18 +26,35 @@ let movies = ["CITIZEN KANE", "CAPTAIN MARVEL", "THE GODFATHER", "SPIRITED AWAY"
 
 let movie = movies[Math.floor(Math.random() * movies.length)];
 console.log(movie)
+let hiddenPhrase = [];
 
 function displayPhrase() {
-    let hiddenPhrase = "";
     for (let i = 0; i < movie.length; i++) {
         if (movie[i] == " ") {
-            hiddenPhrase += "  ";
+            hiddenPhrase.push("  ");
         } 
         else {
-        hiddenPhrase += "_ ";
+        hiddenPhrase.push("_ ");
         };
     };
     console.log(hiddenPhrase);
-    document.getElementById("hiddenPhrase").innerHTML = hiddenPhrase;
+    document.getElementById("hiddenPhrase").innerHTML = hiddenPhrase.join("");
 };
 
+function checkCharacter(value){
+    if (movie.includes(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function revealCharacter(value) {
+    for (let i = 0; i < movie.length; i++) {
+        if (movie[i] == value) {
+            hiddenPhrase.splice(i, 1, value);
+        }
+    }
+    document.getElementById("hiddenPhrase").innerHTML = hiddenPhrase.join("");
+
+}
